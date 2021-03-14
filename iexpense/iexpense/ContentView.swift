1f3832ff821e5146430fb0c977ed67ed554b84f9
sandonl <sandonnlai@gmail.com>
2021-03-15 01:14:28 +1100

    day 36

---
+++ iexpense/iexpense/ContentView.swift
@@ -0,0 +1,54 @@
+//
+//  ContentView.swift
+//  iexpense
+//
+//  Created by Sandon Lai on 13/3/21.
+//
+
+import SwiftUI
+
+struct ExpenseItem {
+    
+    let name: String
+    let type: String
+    let amount: Int
+    
+}
+
+class Expenses: ObservableObject {
+    @Published var items = [ExpenseItem]()
+}
+
+struct ContentView: View {
+    
+    @ObservedObject var expenses = Expenses()
+    
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(expenses.items, id: \.name) { item in
+                    Text(item.name)
+                }
+                .onDelete(perform: removeItems)
+            }
+            .navigationBarTitle("iExpense")
+            .navigationBarItems(trailing: Button(action: {
+                let expense = ExpenseItem(name: "Test", type: "Personal", amount: 5)
+                self.expenses.items.append(expense)
+            }, label: {
+                Image(systemName: "plus")
+            }))
+        }
+    }
+    
+    func removeItems(at offsets: IndexSet) {
+        expenses.items.remove(atOffsets: offsets)
+    }
+    
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iexpense/iexpense/iexpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iexpenseApp.swift
+//  iexpense
+//
+//  Created by Sandon Lai on 13/3/21.
+//
+
+import SwiftUI
+
+@main
+struct iexpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
